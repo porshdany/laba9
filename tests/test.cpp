c194919c3fa2fe53bd9285dd23d6d44f5d94b19b
porshdany <57595417+porshdany@users.noreply.github.com>
2020-05-15 23:34:27 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,22 +1,7 @@
 // Copyright 2020 Usman Turkaev
 #include <gtest/gtest.h>
-#include <webcrawler.hpp>
-
-using namespace webcrawler;
-
 TEST(Example, EmptyTest) {
-  std::string url;
-  std::cout << "Enter url : https://";
-  std::cin >> url;
-  url = "https://" + url;
-  directory_manager manager;
-  char curl[url.size() + 1];
-  std::strcpy(curl, url.c_str());
-  std::string filename = download_obj(curl, manager, "html");
-  std::string html_code = html::read_html_file(filename);
-  std::vector<std::string> vec = html::parser::find_links(html_code);
-  for (const auto& v: vec) {
-    std::cerr << v << std::endl;
-  }
-  EXPECT_TRUE(true);
+    EXPECT_TRUE(true);
 }
+
+
